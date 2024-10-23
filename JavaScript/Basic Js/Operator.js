92df47a8c5c4c44f5d447d91b2ce7caade4208dc
Aman kumar Prajapati <amanprajapati7266@gmail.com>
2024-10-23 18:12:03 +0530

    add

---
+++ JavaScript/Basic Js/Operator.js
@@ -0,0 +1,52 @@
+
+// 1. Arithmetic Operators
+ 
+// let a = 5
+// let b = 10
+// let c = a + b
+// let d = a - b
+// let s = (a+b) + (a-b)
+// let e = a * b
+// let f = a / b
+
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(s)
+
+// 2. Comparison Operators
+
+// let a = 5
+// let b = 10
+// let c = a > b
+// let d = a < b
+// let e = a >= b
+// let f = a <= b
+// let g = a == b
+// let h = a != b
+
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+
+
+// 3. conditional Operators
+//  condition ? true : false
+  
+// let age = 18
+// let message = (age > 18) ? "you can vote" : "you can not vote"
+// console.log(message)
+
+let score = 100
+
+let grade = (score >=90) ? 'A' :
+            (score >=80) ? 'B' :
+            (score >=70) ? 'C' : 'False'
+
+            console.log(grade)
+           
+            
---
+++ JavaScript/Basic Js/basic.js
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+
+//  let method 
+
+// let a = 125
+//   a = 50
+// console.log(a)
+
+//   {
+//     let a = "divya"
+//     console.log(a)
+//   }
+
+// const a = "shakshi"
+// const a ="divya"
+// console.log(a)
+
+// var a = "shakshi"
+// var a = "divya"
+// console.log(a)
+ 
+
+// var a = "shakshi"
+// var b = 135
+// var a = "divya"
+
+// console.log(a,b,a)
+
+var a = "shakshi"
+console.log(a)
+{
+    var a = "divya"
+    console.log(a)
+}
+console.log(a)
