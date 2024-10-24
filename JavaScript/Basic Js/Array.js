2f4c4bbc3b02896b504414bfe780a53976594803
Aman kumar Prajapati <amanprajapati7266@gmail.com>
2024-10-24 18:13:37 +0530

    add

---
+++ JavaScript/Basic Js/Array.js
@@ -0,0 +1,37 @@
+// Array => store the different data type is call a array in JavaScript
+
+// let divya = [1,2,3,"sakshi",true]
+// console.log(divya)
+// console.log(divya.length)
+
+// console.log(divya[2])
+// console.log(divya[3])
+// console.log(divya.length-1)
+
+// Method of a Array => 
+// 1. push => add the value in the last index
+// 2. pop => remove the value in the last index
+// 3. shift => remove the value in the first index
+// 4. unshift => add the value in the first index
+// 5. slice => remove the value in the array
+
+let sakshi = [1,2,3,4,5,6,7,8,9,10]
+//  let divya = sakshi.pop()
+//  console.log(sakshi)
+// //  console.log(divya)
+
+
+
+//  sakshi.push("jaipur")
+//  console.log(sakshi)
+
+// sakshi.shift()
+// sakshi.unshift("divya")
+// console.log(sakshi)
+    // index = 0,1,2,3,4,5,6,7 
+
+let govind = [1,2,3,4,5,6,"divya","sakshi"]
+// console.log(govind.length)
+let aman = govind.slice(1,3)
+console.log(aman)
+// console.log(govind)
--- JavaScript/Basic Js/Condition.js
+++ JavaScript/Basic Js/Condition.js
@@ -19,17 +19,40 @@
 // }
 
 
-let a = 5
-let b = 10
-let c = 20
+// let a = 5
+// let b = 10
+// let c = 20
 
-if(a>b && a>c){
-    console.log("a is greater")
-}
-else if(b>c){
-    console.log("b is greater")
-}
-else{
-    console.log("c is greater")
-}
+// if(a>b && a>c){
+//     console.log("a is greater")
+// }
+// else if(b>c){
+//     console.log("b is greater")
+// }
+// else{
+//     console.log("c is greater")
+// }
 
+// Switch case
+
+// let num = 6
+// switch(num){
+//     case 1:
+//         console.log("One")
+    
+//     case 2:
+//         console.log("Two")
+        
+//     case 3:
+//         console.log("Three")
+//         break
+//     case 4:
+//         console.log("Four")
+//         break
+//     default:
+//         console.log("Invalid")
+// }
+
+
+
+// Calculator code using switch case
