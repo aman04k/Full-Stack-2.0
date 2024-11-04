bfbb4428fde86522cdcba21a5257c3f0a07cc603
Aman kumar Prajapati <amanprajapati7266@gmail.com>
2024-11-04 16:45:02 +0530

    Add

---
+++ JavaScript/Basic Js/Function.js
@@ -0,0 +1,68 @@
+// function num(a, b){
+//     console.log(a+b)
+// }
+// num(10,20)
+
+
+// let name = function(){
+//     console.log("Hello World")
+// }
+// console.log(name())
+
+
+// Squre Program 
+
+// let  squ = function(number){
+//     return number * number
+// }
+// let result = squ(5)
+// console.log(result)
+
+
+// let ram = {
+//     name : "ram",
+//     age : 20,
+//     hight: 5.5,
+//     weight: 50,
+//     add:function (a,b){
+//         console.log(a+b)
+
+//     }
+// }
+// ram.add(10,30)
+
+
+// console.log(this)
+
+// let ram = {
+//     name : "ram",
+//     age : 20,
+//     color: "black",
+//     hight: 5.5,
+//     weight: 50,
+//     isMarried: true,
+//     add: function (){
+//         console.log(this.name + " " + this.age + " " + this.color)
+
+//     }
+// }
+// ram.add()
+
+
+// Arrow Function 
+
+// const functionName = (parameters) => {
+// code or function body
+// }
+
+
+// const sqr = (number) => number* number
+// console.log(sqr(5))
+
+// const  great = (name) => {
+//     return `hello ${name}`
+// }
+// console.log(great("sakshi"))
+
+let number = 10
+console.log(`this is ${number}`)
---
+++ JavaScript/DOM/setTimeout.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+const timer = setTimeout(()=>{
+    console.log("Hello Jaipur")
+},2000)
+
+const timer2 = setTimeout(()=>{
+    console.log("Divya")
+},4000)
+
+setTimeout(()=>{
+    console.log(8+8)
+},3000)
+
+const timer3 = setTimeout(()=>{
+    console.log("Shakshi")
+},6000)
